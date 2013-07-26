77b9b4b508a0a8fd81642f4f202a2653acdc269e
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2013-07-26 16:51:04 -0400

    Update to transcriptome assembly pipeline.
         Step 1) Still cutadapt and FastQC
    NEW  Step 2) Use FLASH to merge paired-end reads that overlap. Reduces total reads while extending length
         Step 3) Use 'diginorm' to normalize reads to further reduce read redundancy and eliminate errors
    
    Next...assemble with velvet. Should take MUCH less memory after data reduction steps

---
+++ projects/ApTranscriptome/scripts/02-A22-FLASH.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###################################################
+## Script to merge paired reads using [FLASH](http://sourceforge.net/projects/flashpage/)
+## John Stanton-Geddes
+## 2013-07-26
+####################################################i
+
+indir="/home/projects/climate-cascade/projects/ApTranscriptome/results/01-trimclip"
+outdir="/home/projects/climate-cascade/projects/ApTranscriptome/data/merged-interleaved"
+
+# Set variables
+samples=(A22-00 A22-03 A22-07 A22-10 A22-14 A22-17 A22-21 A22-24 A22-28 A22-31 A22-35 A22-38)
+
+# Make output directory for files
+mkdir -p $outdir
+
+# Loop across samples, running FLASH to merge reads. Specify phred-offset 33, interleaved output for velvet, and output-prefix to samp
+for samp in "${samples[@]]}"
+do
+    echo "Running FLASH for $samp"
+    flash --phred-offset 33 --interleaved-output --output-directory $outdir --output-prefix $samp ${indir}/${samp}-R1_val_1.fq ${indir}/${samp}-R2_val_2.fq
+    date
+    eacho "$samp done"
+done
---
+++ projects/ApTranscriptome/scripts/03-A22-diginorm.sh
@@ -0,0 +1,36 @@
+#!/obin/bash
+
+####################################################################################################
+## Script to run [digital normalization](http://ged.msu.edu/papers/2012-diginorm/) for mRNAseq data
+## John Stanton-Geddes
+## 2013-07-26
+#####################################################################################################
+
+indir="/home/projects/climate-cascade/projects/ApTranscriptome/data/merged-interleaved"
+outdir="/home/projects/climate-cascade/projects/ApTranscriptome/data/diginorm"
+
+# Set variables
+samples=(A22-00 A22-03 A22-07 A22-10 A22-14 A22-17 A22-21 A22-24 A22-28 A22-31 A22-35 A22-38)
+
+# Make output directory for files
+mkdir -p $outdir
+
+# Loop across merged, running `normalize-by-median` with coverage threshold of 20 and kmer of 21. -N 4 -x 2e9 allocates up to 8GB RAM. 
+for samp in "${samples[@]]}"
+do
+    /opt/software/khmer/scripts/normalize-by-median.py -R diginorm.out -C 20 -k 21 -N 4 -x 2e9 $indir/${samp}.extendedFrags.fastq
+    mv ${samp}.extendedFrags.fastq.keep $outdir/.
+    date
+done
+
+# Loop across notCombined reads, same as before but inclue `p` flag. 
+for samp in "${samples[@]]}"
+do
+    /opt/software/khmer/scripts/normalize-by-median.py -R diginorm.out -C 20 -k 21 -N 4 -x 2e9 -p $indir/${samp}.notCombined.fastq
+    mv $samp/notCombined.fastq.keep $outdir/.
+    date
+done
+
+# One final normalization over all files. Increased memory allocation
+/opt/software/khmer/scripts/normalize-by-median.py -R diginorm-final.out -C 20 -k 21 -N 4 -x 4e9 $outdir/A22*.
+date
