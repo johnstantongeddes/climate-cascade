ed404cc48b8b1f7e5639d85f6076462878d72e3a
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2013-08-14 16:57:03 -0400

    Debugging transcriptome assembly simulation

--- projects/ApTranscriptome/scripts/sim-transcriptome.sh
+++ projects/ApTranscriptome/scripts/sim-transcriptome.sh
@@ -6,37 +6,48 @@
 ## Created: 2013-08-12
 ###################################################################################################
 
+# Set PYTHONPATH to khmer module
+export PYTHONPATH=/opt/software/khmer/python
+
 # File with *Arabidopsis* mRNA transcripts haphazardly selected from [European Nucleotide Archive](http://www.ebi.ac.uk/ena/home)
 
 simfasta="/home/projects/climate-cascade/projects/ApTranscriptome/data/ENA-arabidopsis-random-mRNA.fasta"
+datadir="/home/projects/climate-cascade/projects/ApTranscriptome/data" 
 
-# Assign expression-level to each sequence using the `sel` tool in [rlsim](https://github.com/sbotond/rlsim)
+## Assign expression-level to each sequence using the `sel` tool in [rlsim](https://github.com/sbotond/rlsim)
 
-sel -d "1.0:n:(1500, 3)" ../data/ena.fasta > ../data/ena-simulated.fasta
+sel -d "1.0:g:(1500, 3)" $simfasta > ../data/ena-simulated.fasta
+echo "Done with expression levels: " `date`
 
-# Simulate fragments created during library prep using `rlsim`. Based on length of fragments estimated when using FLASH to pair reads, use empirical length distribution of 180 bp (SD: 20bp).
+## Simulate fragments created during library prep using `rlsim`. Based on length of fragments estimated when using FLASH to pair reads, use empirical length distribution of 180 bp (SD: 20bp).
 
-rlsim -v -n 1000 -d "1:n:(180, 20, 100, 500)" ../data/simulated.fasta > simulated-frags.fasta
+rlsim -v -n 100000 -d "1:n:(180, 20, 100, 500)" ../data/ena-simulated.fasta > ../data/ena-simulated-frags.fasta
+echo "Done with simulated fragmentation levels: " `date`
 
-# Generate simulated Illumina paired-end reads using [simNGS](http://www.ebi.ac.uk/goldman-srv/simNGS/)
+## Generate simulated Illumina paired-end reads using [simNGS](http://www.ebi.ac.uk/goldman-srv/simNGS/)
 # output is 'reads_end1.fq' and 'reads_end2.fq'
+cat ../data/ena-simulated-frags.fasta | simNGS -p paired -o fastq -O reads /opt/software/simNGS/data/s_3_4x.runfile
+# Move files to data directory
+mv reads_end[12].fq ../data/.
+echo "Done with Illumina read simulation: " `date`
 
-cat simulated-frags.fasta | simNGS -p paired -o fastq -O reads /opt/software/simNGS/data/s_3_4x.runfile > ../data/sim-reads.fastq
+## Run through TrimGalore for adapter cutting and quality trimming
+trim_galore --quality 20 --phred33 --fastqc_args "--o $datadir" --length 20 --paired --output_dir $datadir $datadir/reads_end1.fq $datadir/reads_end2.fq
+echo "Done with quality trimming: " `date`
 
-# Run through TrimGalore for adapter cutting and quality trimming
+## Run digital normalization with khmer
+# interleave file
+/opt/software/khmer/sandbox/interleave.py $datadir/reads_end1_val_1.fq $datadir/reads_end2_val_2.fq > ../data/sim-interleaved.fastq
 
-simdir="/home/projects/climate-cascade/projects/AphTranscriptome/data" 
-
-trim_galore --quality 20 --phred33 --fastqc_args "--o $simdir" --length 20 --paired --output_dir $simdir $simdir
-
-# Run digital normalization with khmer
-
+# run diginorm
+/opt/software/khmer/scripts/normalize-by-median.py -R diginorm-final.out -C 20 -k 21 -N 4 -x 4e9 $datadir/sim-interleaved.fastq
+echo "Done with interleaving files:" `date`
 
 # Merge reads with FLASH
-
+flash --phred-offset 33 --interleaved-input --interleaved-output --output-directory $datadir $datadir/sim-normalized.fastq
 
 # Assemble transcriptome with velvet-oases
-
+#velveth 
 
 # Map simulated reads to velvet-oases transcriptome with BWA
 
